d8d6d911d42c7e744e95fa422e5f524d65953ca3
Bemootzer <parzival@trio.li>
2019-03-22 19:47:35 +0100

    Routing and basic Views

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { ViewMapComponent } from './view-map/view-map.component';
+import { ViewHomeComponent } from './view-home/view-home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: ViewHomeComponent},
+  {path: 'map', component: ViewMapComponent},
+  {path: '**', component: ViewHomeComponent},
+  { path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,23 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ViewMapComponent } from './view-map/view-map.component'
+import { MatMenuModule } from '@angular/material/menu';
+import { ViewHomeComponent } from './view-home/view-home.component'; 
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ViewMapComponent,
+    ViewHomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
